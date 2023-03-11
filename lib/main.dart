f3b630311df46887f1e1932a883139bfceffe49e
Nikita Menshikov <menshikov.mikita@gmail.com>
2023-03-11 14:28:31 +0300

    Added firebase initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,22 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'resources/themes.dart';
 import 'router/app_router.dart';
 import 'view/home_screen/home_screen.dart';
 
 void main() {
-  runApp(PokerApp(
-    appRouter: AppRouter(),
-  ));
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight,
+  ]).then((_) async {
+    await Firebase.initializeApp();
+    runApp(PokerApp(
+      appRouter: AppRouter(),
+    ));
+  });
 }
 
 class PokerApp extends StatelessWidget {
