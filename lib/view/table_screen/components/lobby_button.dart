0da231fa9c58a356ee586ada9605cd41ee8d73aa
Nikita Menshikov <menshikov.mikita@gmail.com>
2023-03-23 18:24:35 +0300

    Modified lobby button styles

--- lib/view/table_screen/components/lobby_button.dart
+++ lib/view/table_screen/components/lobby_button.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../../resources/colors.dart';
+
 class LobbyButton extends StatelessWidget {
   final String _text;
   final Function() _onPressedFunc;
@@ -10,10 +12,39 @@ class LobbyButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var height = MediaQuery.of(context).size.height;
     return Expanded(
-      child: ElevatedButton(
-        onPressed: _onPressedFunc,
-        child: Text(_text),
+      child: Container(
+        height: 0.07 * height,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              greyColor.withOpacity(0.9),
+              greyColor.withOpacity(0.9),
+              greyColor,
+              greyColor,
+              greyColor.withOpacity(0.9),
+            ],
+          ),
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+        child: ElevatedButton(
+          onPressed: _onPressedFunc,
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.transparent,
+            shadowColor: Colors.transparent,
+            elevation: 5.0,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+          ),
+          child: Text(
+            _text,
+            style: Theme.of(context).textTheme.labelLarge,
+          ),
+        ),
       ),
     );
   }
